fb1bd709889872aa3f892e2ada9753284bcd5b26
Sirong Huang <huangsirong111@hotmail.com>
2024-07-14 04:17:25 +0300

    milestone: rewrite with the following changes: 1. use chromaDB persistent client 2. detailed runtime printing for latency optimization 3. allow both create/update and loading the DB

--- colearner/rag.py
+++ colearner/rag.py
@@ -1,39 +1,73 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 import streamlit as st
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
+import chromadb
+from colearner.utils import runtime
+import time
 
 
-@st.cache_resource(ttl="1h")
-def configure_retriever(_docs):
+@runtime
+@st.spinner("Processing data for your Chatbot...")
+def configure_retriever(docs:list = [], hashes:list = [], update:bool = False):
     """
     Configure retriever for RAG model. Split documents, create embeddings and store in vectordb, and define retriever.
-    
-    - Input: list of documents
-    - Output: retriever object
-    
     - Splitter: RecursiveCharacterTextSplitter
     - Embeddings: HuggingFaceEmbeddings
     - Vectordb: ChromaDB (save to disk if CHROMADB_PATH provided as env variable, otherwise stores in memory)
     - Retrieval: mmr
+    ---------------------------------------------------
+    - Input: 
+        - _docs: list of documents
+        - hashes: list of hashes of the documents used as unique ids
+        - update (default=True): if True, create or update the vectordb with new documents, otherwise load the existing vectordb. 
+    - Output: retriever object
     """
+    # Define the chromaDB client
+    start_time = time.time()
     
-    # Split documents
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_documents(_docs)
-
-    # Create embeddings and store in vectordb
     embedding_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    if os.getenv('CHROMADB_PATH'):
-        vectordb = Chroma.from_documents(splits, embedding_function, persist_directory=os.getenv('CHROMADB_PATH'))
-
-    else: 
-        vectordb = Chroma.from_documents(splits, embedding_function)
-
-    # Define retriever
+    elapsed_time = time.time() - start_time
+    print(f"Elapsed time for embedding_function: {elapsed_time} seconds")
+    
+    start_time = time.time()
+    
+    persistent_client = chromadb.PersistentClient(path=os.getenv('CHROMADB_PATH'))
+    elapsed_time = time.time() - start_time
+    print(f"Elapsed time for persistent_client: {elapsed_time} seconds")
+    
+    start_time = time.time()
+    
+    vectordb = Chroma(
+        client=persistent_client,
+        collection_name="collection_name",
+        embedding_function=embedding_function,
+    )
+    
+    elapsed_time = time.time() - start_time
+    print(f"Elapsed time for creating vectordb: {elapsed_time} seconds")
+    
+    if update:
+        start_time = time.time()
+        
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        splits = text_splitter.split_documents(docs)
+        
+        elapsed_time = time.time() - start_time
+        print(f"Elapsed time for creating text splittings: {elapsed_time} seconds")
+        
+        start_time = time.time()
+        vectordb.add_documents(ids=hashes, documents=docs)
+        
+        elapsed_time = time.time() - start_time
+        print(f"Elapsed time for adding documents to vectordb: {elapsed_time} seconds")
+    
+    
     retriever = vectordb.as_retriever(search_type="mmr", search_kwargs={"k": 2, "fetch_k": 4})
-
+    
+    print("Retriever configured successfully!")
+    
     return retriever
+
+
