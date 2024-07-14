46f29cc4c31eb29f3c54630c0cc3645b0e105180
Sirong Huang <huangsirong111@hotmail.com>
2024-07-14 04:18:15 +0300

    refactor: doc->docs small naming change

--- colearner/pdf_loader.py
+++ colearner/pdf_loader.py
@@ -3,5 +3,6 @@ from langchain_community.document_loaders import PyPDFLoader
 def load_pdf(pdf_path:str) -> list:
     """Load a PDF file and return a list of documents."""
     loader = PyPDFLoader(pdf_path)
-    doc = loader.load()
-    return doc
+    docs = loader.load()
+    return docs
+
