eeb18b46d510dfdef22fbcf22fd4ca378daa6298
Sirong Huang <huangsirong111@hotmail.com>
2024-07-14 04:22:48 +0300

    milestone: rewrite with the following features: 1.use user updated PDF rather than simple toy examples 2. check for file duplicates (requires fixing: should check vs vectorDB id instead of uploaded file hashes) 3. retriever only reconfigures on non-duplicated new file upload(again should check vs vectorDB) 4. added detailed debugging prints 5. retriever stored in sesstion_state to avoid unwanted behavior on app refreshes

--- app.py
+++ app.py
@@ -1,35 +1,80 @@
+import os
 from dotenv import load_dotenv
-from openai import chat
 load_dotenv()
 
 from colearner import chatbot
-from langchain_core.documents import Document  
+from colearner.utils import create_folder, save_file, get_file_hash
+from colearner.pdf_loader import load_pdf 
 from colearner.rag import configure_retriever
 from colearner.chatbot import Context_with_History_Chatbot
 
 import streamlit as st
 
+debug = True
+if debug:
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~NEW RUN~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
+##### Create local PDF file dir ##### 
+save_file_dir = os.getenv('DATA_FOLDER_PATH') + "uploaded_files/pdf/"
+create_folder(save_file_dir)    
+
+##### Setup Streamlit UI ##### 
 st.set_page_config(page_title="CoLearner: Chat with your documents", page_icon="🧙")
-st.title("🧙 CoLearner: Chat with your documents")
+st.header("🧙 CoLearner: Chat with your documents")
 
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+##### User uploading PDFs ##### 
+uploaded_files = st.sidebar.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)                                                                # stop running the rest of the script if no files are uploaded
 
-debug = True
+##### Setup session states #####
+if 'uploaded_file_hashes' not in st.session_state:                            # initiate a session state variable for storing hashes of uploaded files
+    st.session_state.uploaded_file_hashes = []
+                                                  
+if "retriever" not in st.session_state:                                       # initiate a session state variable for storing the retriever
+    st.session_state.retriever = configure_retriever(update=False)            # initiate the retriever for the first time
+    if debug:
+        print("=======   Retriever configured for the first time   =======", '\n')
 
-# some toy data 
-pdfs = [Document(page_content="My mom's name is Qiu", metadata={"source": "https://mom.com"}),
-        Document(page_content="My dad's name is Jun", metadata={"source": "https://dad.com"}),
-        Document(page_content="My dog's name is Chong", metadata={"source": "https://dog.com"})]
+##### Check for duplicates and save to disk #####    
+if uploaded_files:                                                            # when files are uploaded (also when the page is refreshed and there are uploaded files)                                            
+    new_file_hash = get_file_hash(uploaded_files[-1])                         # get the hash of the last uploaded file
 
-retriever = configure_retriever(pdfs)
+    if new_file_hash in st.session_state.uploaded_file_hashes:                # --> duplicated upload (avoids re-processing due to duplicated uploads or app refreshes)           
+        pass                                                         
+    else:                                                                     # --> new upload                                                                  
+        st.session_state.uploaded_file_hashes.append(new_file_hash)           # add the new file hash to session state variable
+        
+        file_path = save_file_dir + uploaded_files[-1].name
+        save_file(uploaded_files[-1], file_path)                              # save the file locally
 
-chatbot = Context_with_History_Chatbot()
-final_chain = chatbot.get_qa_chain(retriever)
+        new_pdf_doc = load_pdf(file_path)                                     # load the new PDF as langchain document    
+        
+        if debug == True:
+            print("=======    id: hashes (should be list of hashes)  =======", '\n')
+            print([new_file_hash])
+            print("=======    docs (should be list of docs)  =======", '\n')
+            print(new_pdf_doc)                                  
+        
+        st.session_state.retriever = configure_retriever(                     # update the ChromaDB and retriever with the new PDF
+                                            hashes = [new_file_hash],                 
+                                            docs = new_pdf_doc, 
+                                            update=True)
+        
+    if debug == True:
+        print("=======    Uploaded_files   =======", '\n')
+        print(uploaded_files)
+        print("=======    Embeddings in VectorDB   =======", '\n')
+        print(st.session_state.retriever.vectorstore.get())
+        print("=======    Session states   =======", '\n')
+        print(st.session_state)    
+        
+##### Create Chatbot #####    
+chatbot = Context_with_History_Chatbot(model = "gpt-3.5-turbo")
+final_chain = chatbot.get_qa_chain(st.session_state.retriever)
 
-
+##### Chat interface #####
 if user_query := st.chat_input(placeholder="Ask me anything!"):
     st.chat_message("human", avatar=chatbot.avatars["human"]).write(user_query)
     
@@ -44,5 +89,8 @@ if user_query := st.chat_input(placeholder="Ask me anything!"):
         print(chatbot.relevant_context,'\n')
         print("======= Chat History =======",'\n')
         for message in chatbot.msgs.messages:
-            print(message.content)  #Fix: not sure why there is a double printing behavior here... 
+            print(message.content)  
         print("##############################################################################################")
+
+# if st.button('Reset vector database'):
+#     pass
