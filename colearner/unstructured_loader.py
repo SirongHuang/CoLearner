f2c257139dc9d4b0ca489dc053a1657c904eb315
Sirong Huang <huangsirong111@hotmail.com>
2024-07-17 21:03:37 +0300

    refactor: changed function name

--- colearner/unstructured_loader.py
+++ colearner/unstructured_loader.py
@@ -3,7 +3,7 @@ from colearner.utils import runtime
 from langchain_community.document_loaders import UnstructuredFileLoader
 
 @runtime
-def load_unstructured_file(file_paths):
+def unstructured_loader(file_paths):
     """ Load unstructured files into Langchain Documents from the given list of paths. """
     
     loader = UnstructuredFileLoader(file_paths, mode="single")
